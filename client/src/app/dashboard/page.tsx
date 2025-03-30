98e89e740e34cce2b4e901812840c13c3e9e57a1
Lokesh <lokeshnegi649@gmail.com>
2025-03-30 20:17:38 +0530

    add CardExpenseSummary, CarPurchaseSummary and CardSalesSummary

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -2,11 +2,17 @@
 
 import React from "react";
 import CardPopularProducts from "./CardPopularProducts";
+import CardSalesSummary from "./CardSalesSummary";
+import CardPurchaseSummary from "./CardPurchaseSummary";
+import CardExpenseSummary from "./CardExpenseSummary";
 
 const page = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
       <CardPopularProducts />
+      <CardSalesSummary />
+      <CardPurchaseSummary />
+      <CardExpenseSummary />
     </div>
   );
 };
