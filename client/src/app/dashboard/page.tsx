331c734d512ebc5d7b1a46d5155fe91a3f11b814
Lokesh <lokeshnegi649@gmail.com>
2025-03-31 23:57:30 +0530

    add stats cards on dashboard

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -5,6 +5,8 @@ import CardPopularProducts from "./CardPopularProducts";
 import CardSalesSummary from "./CardSalesSummary";
 import CardPurchaseSummary from "./CardPurchaseSummary";
 import CardExpenseSummary from "./CardExpenseSummary";
+import StatCard from "./StatCard";
+import { Package, TrendingDown, TrendingUp } from "lucide-react";
 
 const page = () => {
   return (
@@ -13,6 +15,25 @@ const page = () => {
       <CardSalesSummary />
       <CardPurchaseSummary />
       <CardExpenseSummary />
+      <StatCard
+        title="Customer & Expenses"
+        primaryIcon={<Package className="text-blue-600 w-6 h-6" />}
+        dateRange="22 - 29 October 2023"
+        details={[
+          {
+            title: "Customer Growth",
+            amount: "175.00",
+            changePercentage: 131,
+            IconComponent: TrendingUp,
+          },
+          {
+            title: "Expenses",
+            amount: "10.00",
+            changePercentage: -56,
+            IconComponent: TrendingDown,
+          },
+        ]}
+      />
     </div>
   );
 };
